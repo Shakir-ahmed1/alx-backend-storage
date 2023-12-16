ddc53dbea10c878df6b90215288ba3fbf1a34f6a
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-12-16 22:09:14 +0000

    exchangin if statment

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- displays bands with style of Galm rock and their lifespan
 SELECT band_name,
-       IF(split IS NULL, 2020 - formed , split - formed) AS lifespan
+       IF(split IS NOT NULL, split - formed, 2022 - formed) AS lifespan
 FROM metal_bands
 WHERE style REGEXP "Glam rock"
-ORDER BY lifespan DESC
+ORDER BY lifespan DESC;
