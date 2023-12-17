6dd3d47a602be604144c2b2fe51bd152b19c73b9
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-12-17 01:10:50 +0000

    returning float instead of decimal

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,16 +1,12 @@
 -- dividing safe mode
 DELIMITER $$
 CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS DECIMAL(10, 6)
+RETURNS FLOAT
 BEGIN
-    DECLARE result DECIMAL(10, 6);
-
-    IF b <> 0 THEN
-        SET result = a / b;
+    IF b = 0 THEN
+        RETURN 0;
     ELSE
-        SET result = 0;
+        RETURN a / b;
     END IF;
-
-    RETURN result;
 END$$
 DELIMITER ;
