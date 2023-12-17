c9c1066e4aa18cddeff78091939ae9e8079f2109
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-12-17 01:31:43 +0000

    fixing correct output using temporary table

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,35 +1,28 @@
 -- calculates the weighted average for all users
 DELIMITER //
-
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-    DECLARE user_id_param INT;
-    
-    -- Declare a cursor to iterate over user IDs
-    DECLARE user_cursor CURSOR FOR
-        SELECT id FROM users;
-    
-    -- Open the cursor
-    OPEN user_cursor;
-    
-    -- Start fetching user IDs
-    user_loop: LOOP
-        -- Fetch the next user ID
-        FETCH user_cursor INTO user_id_param;
-        
-        -- Exit the loop if no more rows to fetch
-        IF user_id_param IS NULL THEN
-            LEAVE user_loop;
-        END IF;
-        
-        -- Call the ComputeAverageWeightedScoreForUser procedure for the current user
-        CALL ComputeAverageWeightedScoreForUser(user_id_param);
-    END LOOP;
-    
-    -- Close the cursor
-    CLOSE user_cursor;
-    
-END //
+    -- Temporary table to store weighted scores
+    CREATE TEMPORARY TABLE IF NOT EXISTS weighted_scores (
+        user_id INT NOT NULL,
+        weighted_score FLOAT DEFAULT 0,
+        total_weight INT DEFAULT 0
+    );
 
+    -- Calculate weighted scores for each user and project
+    INSERT INTO weighted_scores (user_id, weighted_score, total_weight)
+    SELECT c.user_id, SUM(c.score * p.weight) AS weighted_score, SUM(p.weight) AS total_weight
+    FROM corrections c
+    JOIN projects p ON c.project_id = p.id
+    GROUP BY c.user_id;
+
+    -- Update the average_score in the users table
+    UPDATE users u
+    JOIN weighted_scores ws ON u.id = ws.user_id
+    SET u.average_score = ws.weighted_score / ws.total_weight;
+
+    -- Drop the temporary table
+    DROP TABLE weighted_scores;
+END //
 DELIMITER ;
 
