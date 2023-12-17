65d3940c11fdc7ddd9b4859787fedf07000bd0ed
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-12-17 00:03:08 +0000

    handling Null values

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,9 +3,12 @@ DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(user_id INT)
 BEGIN
     DECLARE av DECIMAL;
-    select AVG(score) INTO av FROM corrections c WHERE c.user_id = user_id GROUP BY c.user_id;
+    SELECT AVG(score) INTO av
+    FROM corrections c
+    WHERE c.user_id = user_id;
+
     UPDATE users
-    SET average_score = av
+    SET average_score = IFNULL(av, 0)
     WHERE id = user_id;
 END$$
 DELIMITER ;
