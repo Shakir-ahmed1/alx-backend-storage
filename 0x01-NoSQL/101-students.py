69d238205ed4e33a769aef6106e4ca9f631141bd
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-12-19 15:56:11 -0800

    Top students

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" insert a document in python """
+
+
+def top_students(mongo_collection):
+    """ insert to school"""
+    data = mongo_collection.find()
+    result = []
+    for d in data:
+        total_score = 0
+        for t in d.get('topics', []):
+            total_score += t.get('score', 0)
+        d['averageScore'] = total_score / len(d.get('topics', []))
+        result.append(d)
+    return result
