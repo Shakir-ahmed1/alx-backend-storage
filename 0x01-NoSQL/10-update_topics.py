4d199106059c2d453b226d906b4552038d05252f
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-12-19 15:22:34 -0800

    pycodestyle fix

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """ update a document in python """
+
+
 def update_topics(mongo_collection, name, topics):
     """ update documents's topics """
-    mongo_collection.update_many({"name": name}, {"$set": {"topics" :topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
