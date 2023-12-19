856fcbb9018e8a5fe8bf47cc877544c23754c6c6
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-12-19 14:59:50 -0800

    insert school

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 """ insert a document in python """
+
+
 def insert_school(mongo_collection, **kwargs):
+    """ insert to school"""
     result = mongo_collection.insert_one(kwargs)
     return result.inserted_id
