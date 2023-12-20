759d1197e6d47a6c7210990238ec3414e1ea92f7
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-12-19 16:44:26 -0800

    log stats

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nb = client.logs.nginx
+    e = {}
+    print(f"{nb.count_documents(e)} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {nb.count_documents({'method':'GET'})}")
+    print(f"\tmethod POST: {nb.count_documents({'method':'POST'})}")
+    print(f"\tmethod PUT: {nb.count_documents({'method':'PUT'})}")
+    print(f"\tmethod PATCH: {nb.count_documents({'method':'PATCH'})}")
+    print(f"\tmethod DELETE: {nb.count_documents({'method':'DELETE'})}")
+    print(f"{nb.count_documents({'method':'GET','path':'/status'})}"
+          f" status check")
