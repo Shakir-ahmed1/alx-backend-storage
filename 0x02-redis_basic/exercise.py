abd4472e63ea4ea0378225246eecd2eacfeeee99
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-12-20 20:17:00 +0000

    radis basics

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,7 +18,7 @@ class Cache:
         self._redis.set(id, data)
         return id
 
-    def get(self, key: str, fn: Union[Callable, None] =None) -> Any:
+    def get(self, key: str, fn: Union[Callable, None] = None) -> Any:
         """ gets the desired element"""
         a = self._redis.get(key)
         if a is None or fn is None:
@@ -36,15 +36,3 @@ class Cache:
         """ gets the desired element"""
         a = self._redis.get(key)
         return int(a)
-
-cache = Cache()
-
-TEST_CASES = {
-    b"foo": None,
-    123: int,
-    "bar": lambda d: d.decode("utf-8")
-}
-
-for value, fn in TEST_CASES.items():
-    key = cache.store(value)
-    assert cache.get(key, fn=fn) == value
