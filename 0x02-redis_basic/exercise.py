f779afbca7213cdac15584069f3af4d848829ace
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-12-20 20:15:13 +0000

    Reading from Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """ writing strings to redis """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Any
 
 
 class Cache:
@@ -17,3 +17,34 @@ class Cache:
         id = str(uuid.uuid4())
         self._redis.set(id, data)
         return id
+
+    def get(self, key: str, fn: Union[Callable, None] =None) -> Any:
+        """ gets the desired element"""
+        a = self._redis.get(key)
+        if a is None or fn is None:
+            return a
+        else:
+            value = fn(a)
+            return value
+
+    def get_str(self, key: str) -> str:
+        """ gets the desired element"""
+        a = self._redis.get(key)
+        return str(a)
+
+    def get_int(self, key: str) -> int:
+        """ gets the desired element"""
+        a = self._redis.get(key)
+        return int(a)
+
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
