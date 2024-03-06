ba7c657c9b0e97dfee050d30e0a4062a06aafa3d
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-03-06 20:12:08 +0300

    adding advanced task

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,33 +1,51 @@
-#!/usr/bin/env python3
-""" A module that implement expiring web cache """
-
-import redis
 import requests
+import redis
+import time
 from typing import Callable
-from functools import wraps
 
-redis = redis.Redis()
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
+def get_page(url: str) -> str:
+    """Fetches the HTML content of a URL and caches it with expiration time of 10 seconds. """
+    cached_content = redis_client.get(url)
+    if cached_content:
+        return cached_content.decode('utf-8')
 
-def wrap_requests(func: Callable) -> Callable:
-    """ decorator wrapper """
+    response = requests.get(url)
+    html_content = response.text
+    redis_client.setex(url, 10, html_content)
+    count_key = f"count:{url}"
+    redis_client.incr(count_key)
 
-    @wraps(func)
-    def wrapper(url):
-        """ wrapper for decorator """
-        redis.incr(f"count:{url}")
-        cached_response = redis.get(f"cached:{url}")
-        if cached_response:
-            return cached_response.decode('utf-8')
-        result = func(url)
-        redis.setex(f"cached:{url}", 10, result)
-        return result
+    return html_content
 
+def cache_and_track(func: Callable) -> Callable:
+    """Decorator for caching and tracking the number of times a URL is accessed."""
+    def wrapper(url: str) -> str:
+        """Wrapper function for caching and tracking."""
+        # Check if the URL content is cached
+        cached_content = redis_client.get(url)
+        if cached_content:
+            return cached_content.decode('utf-8')
+        html_content = func(url)
+        redis_client.setex(url, 10, html_content)
+        count_key = f"count:{url}"
+        redis_client.incr(count_key)
+
+        return html_content
     return wrapper
 
-
-@wrap_requests
-def get_page(url: str) -> str:
-    """get page using url """
+@cache_and_track
+def get_page_cached(url: str) -> str:
+    """Fetches the HTML content of a URL, caching it with an expiration time of 10 seconds. """
     response = requests.get(url)
     return response.text
+
+if __name__ == "__main__":
+    url = "http://slowwly.robertomurray.co.uk/delay/1000/url/https://www.example.com"
+    
+    start_time = time.time()
+    html_content = get_page(url)
+
+    start_time = time.time()
+    html_content_cached = get_page_cached(url)
